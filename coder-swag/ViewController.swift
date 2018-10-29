34faa5736d092dd544da029dec80232d479bca2f
Yairo Fernandez <Yairologo@Yairos-MacBook-Pro.local>
2018-10-29 13:08:08 -0400

    first commit

---
+++ coder-swag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Yairo Fernandez on 10/29/18.
+//  Copyright © 2018 Yairo Fernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
